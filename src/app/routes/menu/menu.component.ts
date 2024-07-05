45d5a58c27cdec90a81e4a683d28e8215a5449ce
Kaic Melo Santos <kakakaic@yahoo.com.br>
2024-07-05 03:10:21 -0300

    feat: creating profile

---
+++ src/app/routes/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [MatButtonModule, MatMenuModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.less',
+})
+export class MenuComponent {
+  router: Router = inject(Router);
+
+  onPlayer(): void {
+    this.router.navigate(['players']);
+  }
+
+  onBattle(): void {
+    this.router.navigate(['battle']);
+  }
+}
