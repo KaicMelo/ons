fb28f54e93d11c3ae9fe2e3d6b0fb440b6ccd83c
Kaic Melo Santos <kakakaic@yahoo.com.br>
2024-07-04 23:57:56 -0300

    feat: install socket io

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { SocketIoConfigModule } from './socket-io-config/socket-io-config.module';
+import { SocketService } from './socket/socket.service';
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SocketIoConfigModule, SocketIoModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
+  providers: [SocketService],
 })
-export class AppComponent {
-  title = 'ons';
+export class AppComponent implements OnInit{
+  message!: string;
+
+  constructor(private socketService: SocketService) {}
+
+  ngOnInit() {
+    this.socketService.getMessage().subscribe((message) => {
+      this.message = message;
+    });
+  }
+
+  sendMessage() {
+    this.socketService.sendMessage('Hello from Angular Standalone Component');
+  }
+
+
 }
---
+++ src/app/socket/socket.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+
+  constructor(private socket: Socket) { }
+
+  sendMessage(message: string) {
+    this.socket.emit('message', message);
+  }
+
+  getMessage(): Observable<string> {
+    return this.socket.fromEvent<string>('message');
+  }
+}
