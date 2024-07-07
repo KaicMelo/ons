ff7118d799a6dec85bb6a6255a3e082e19511d9c
Kaic Melo Santos <kakakaic@yahoo.com.br>
2024-07-07 15:03:25 -0300

    feat: creating brackets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PlayersComponent } from './routes/players/players.component';
 import { RegisterComponent } from './routes/players/register/register.component';
 import { BattleComponent } from './routes/battle/battle.component';
+import { PointsComponent } from './routes/points/points.component';
 
 export const routes: Routes = [
   {
@@ -19,5 +20,9 @@ export const routes: Routes = [
   {
     path: 'battle',
     component: BattleComponent,
+  },
+  {
+    path: 'points',
+    component: PointsComponent,
   },
 ];
